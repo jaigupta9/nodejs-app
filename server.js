522cd61feb0e6ea572a7f8732fb00759e11ef459
Jai Gupta <jaigupta0905@gmail.com>
2025-07-30 11:24:25 +0530

    Your message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = 80;
 
-app.use(express.static("public"));
+app.use(express.static("Frontend"));
 
 app.listen(port, function () {
     console.log("Server is running on http://localhost:" + port);
